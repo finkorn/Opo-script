4153b584f553ede4c2317cb102e97eac13a1e862
finkorn <finkorn@gmail.com>
2013-06-06 15:58:50 +0200

    Update Opo script.js

--- Opo script.js
+++ Opo script.js
@@ -27,7 +27,7 @@ function readCookies() {
     	value = jaaulde.utils.cookies.get(COOKIE_STREAMING);
     	streaming = value != null ? value: true;
     	value = jaaulde.utils.cookies.get(COOKIE_HIDE_VIDEO);
-    	hideVideo = value != null ? value : false;
+    	SkrýtVideo = value != null ? value : false;
     	var value = jaaulde.utils.cookies.get(COOKIE_EMOTES);
     	emotes = value != null ? value : true;
     	var value = jaaulde.utils.cookies.get(COOKIE_AUDIENCE);
